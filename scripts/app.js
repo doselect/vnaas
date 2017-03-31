72f1b1b73b330621911459dcefa8dfa5c7c86d12
Jaipradeesh Janarthanan <jai@doselect.com>
2017-03-31 20:27:57 +0530

    Integrate baz
    Signed-off-by: Jaipradeesh Janarthanan <jai@doselect.com>

--- scripts/app.js
+++ scripts/app.js
@@ -1,36 +1,31 @@
-var bazUrl = "https://us-central1-vnaas-baz.cloudfunctions.net/baz-dev"
+var bazUrl = 'https://us-central1-vnaas-baz.cloudfunctions.net/baz-dev'
 
-$(document).ready(
-function()
-{
-  $('#value').change(function() {
+$(document).ready(function () {
+  $('#case_select').change(function () {
+  // Get category value
+    var name = $('#case_select option:selected').val()
+    var reqParam = {}
 
-  //get category value
-  var name = $("#value option:selected").text();
+    if (name === 'rust' || name === 'csharp' || name === 'go' || name === 'java' || name === 'javascript' || name === 'swift' || name === 'dotnet') {
+      reqParam.case = 'camel'
+    } else if (name === 'fsharp') {
+      reqParam.case = 'pascal'
+    } else if (name === 'python' || name === 'ruby' || name === 'elixir' || name === 'eiffel' || name === 'c' || name === 'perl') {
+      reqParam.case = 'snake'
+    } else if (name === 'lisp') {
+      reqParam.case = 'kebab'
+    } else {
+      reqParam.case = 'camel'
+    }
 
-  if ( name === "Rust" || name === "C#" || name === "Go" || name === "Java" || name === "Javascript" || name === "Swift" || name === ".NET" ) {
-    var result = $.get(bazUrl,"camel")
-  }
+    $.get(bazUrl, reqParam)
+      .success(function (resp) {
+        $('#case_display').text(resp.variable)
+      })
+  })
+})
 
-  if ( name === "F#" ) {
-    var result = $.get(bazUrl,"pascal")
-  }
-
-  if ( name === "Python" || name === "Ruby" || name === "Rust" || name === "Elixir" || name === "Eiffel" || name === "C" || name === "Perl" || ) {
-    var result = $.get(bazUrl,"snake")
-  }
-
-  if ( name === "Lisp" ) {
-    var result = $.get(bazUrl,"kebab")
-  }
-
-  else {
-    var result = $.get(bazUrl,"camel")
-  }
-
-  });
-});
-
+// Case mapping
 // camel: Rust, C#, Go, Java, Javascript, Swift, .NET
 // pascal: F#
 // snake: Python, Ruby, Rust, Elixir, Eiffel, C, Perl
