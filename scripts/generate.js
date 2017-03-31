a1b82d41b74120f8e2a350d70e47749ceb89c5ae
Barkha <barkha@doselect.com>
2017-03-31 18:56:09 +0530

    value of variable case according to language

---
+++ scripts/generate.js
@@ -0,0 +1,33 @@
+$(document).ready(
+function()
+{
+  $('#value').change(function() {
+
+  //get category value
+  name = $("#value option:selected").text();
+
+  if ( name === "Rust" || name === "C#" || name === "Go" || name === "Java" || name === "Javascript" || name === "Swift" || name === ".NET" ) {
+    "camel"
+  }
+
+  if ( name === "F#" || ) {
+    "pascal"
+  }
+
+  if ( name === "Python" || name === "Ruby" || name === "Rust" || name === "Elixir" || name === "Eiffel" || name === "C" || name === "Perl" || ) {
+    "snake"
+  }
+
+  if ( name === "Lisp" || ) {
+    "kebab"
+  }
+
+
+  });
+});
+
+
+// camel: Rust, C#, Go, Java, Javascript, Swift, .NET
+// pascal: F#
+// snake: Python, Ruby, Rust, Elixir, Eiffel, C, Perl
+// kebab: Lisp
